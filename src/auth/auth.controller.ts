e7cc5711f326c85bd54463ff2300ecd4dd8281f7
Priyanka Mehta <primehta17@gmail.com>
2025-12-16 19:39:46 +0530

    user register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
-import { Controller,Post } from '@nestjs/common';
+import { Body, Controller,Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import {RegisterDto} from './dto/registerUser.dto'
 
 @Controller('auth') // /auth/register route
 export class AuthController {
   constructor(private readonly authService: AuthService){}
   @Post('register')
-  register(){
-    const result=this.authService.registerUser();
+  register(@Body() registerUserDto: RegisterDto){
+    const result=this.authService.registerUser(registerUserDto);
      return result;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { RegisterDto } from './dto/registerUser.dto';
 
 @Injectable()
 export class AuthService {
+
   //Logic for user register
   //1. check if email a;ready exists
   //2. hash the password
   //3. store the user into db
   //4. genarate jwt token
   //5. send token in response
-   registerUser(){
+  constructor(private readonly userService: UserService){}
+   registerUser(registerUserDto:RegisterDto){
+    console.log("registerDto",registerUserDto);
      return {message: 'User registerd successfully!'};
   }
 }
